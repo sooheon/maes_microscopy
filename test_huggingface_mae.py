68772890a7b9c625802e074a6a2a80a006899394
Laksh <lakshmanansathya@gmail.com>
2024-11-05 12:35:34 -0500

    add mae model (#15)
    * add mae model
    
    * push to hugging face
    
    * minor cleanup
    
    * more changes
    
    * set mask ratio to 0
    
    * successful validation
    
    * rearrange
    
    * remove testing code
    
    * remove unused funtion
    
    * add a predict method
    
    * update to correct version of phenom-beta
    
    * add Kian's PR suggestion
    
    * add test
    
    * add comment to download model
    
    * add multiple channel test
    
    * allow channelwise embs
    
    * clean some dead code
    
    * add reconstruction notebook with example. can run on CPU no prob
    
    * fix up
    
    * udpate notebook
    
    * remove the need for hydra
    
    ---------
    
    Co-authored-by: Laksh <laksh.arumugam@recursionpharma.com>
    Co-authored-by: kian-kd <kian.kd@recursionpharma.com>

---
+++ test_huggingface_mae.py
@@ -0,0 +1,32 @@
+import pytest
+import torch
+
+from huggingface_mae import MAEModel
+
+huggingface_phenombeta_model_dir = "models/phenom_beta_huggingface"
+# huggingface_modelpath = "recursionpharma/test-pb-model"
+
+
+@pytest.fixture
+def huggingface_model():
+    # Make sure you have the model/config downloaded from https://huggingface.co/recursionpharma/test-pb-model to this directory
+    # huggingface-cli download recursionpharma/test-pb-model --local-dir=models/phenom_beta_huggingface
+    huggingface_model = MAEModel.from_pretrained(huggingface_phenombeta_model_dir)
+    huggingface_model.eval()
+    return huggingface_model
+
+
+@pytest.mark.parametrize("C", [1, 4, 6, 11])
+@pytest.mark.parametrize("return_channelwise_embeddings", [True, False])
+def test_model_predict(huggingface_model, C, return_channelwise_embeddings):
+    example_input_array = torch.randint(
+        low=0,
+        high=255,
+        size=(2, C, 256, 256),
+        dtype=torch.uint8,
+        device=huggingface_model.device,
+    )
+    huggingface_model.return_channelwise_embeddings = return_channelwise_embeddings
+    embeddings = huggingface_model.predict(example_input_array)
+    expected_output_dim = 384 * C if return_channelwise_embeddings else 384
+    assert embeddings.shape == (2, expected_output_dim)
